aecfb477957319e7df4e1ed8c28707e55804aeef
Nicolas <nicolasmeireles1004@gmail.com>
2024-07-07 17:42:37 -0300

    :ok_hand: Initial Project.

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,17 @@
+import Header from "../../components/header/Header.jsx";
+import Nav from "../../components/nav/Nav.jsx";
+import Content from "../../components/content/Content.jsx";
+import Footer from "../../components/footer/Footer.jsx";
+
+function Home() {
+    return (
+        <div>
+            <Header/>
+            <Nav/>
+            <Content/>
+            <Footer/>
+        </div>
+    )
+}
+
+export default Home;
---
+++ src/pages/user/User.jsx
@@ -0,0 +1,15 @@
+import Header from "../../components/header/Header.jsx";
+import Content from "../../components/content/Content.jsx";
+import Footer from "../../components/footer/Footer.jsx";
+
+function User() {
+    return (
+        <div>
+            <Header/>
+            <Content/>
+            <Footer/>
+        </div>
+    );
+}
+
+export default User;
