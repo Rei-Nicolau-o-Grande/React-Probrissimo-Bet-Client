7ae5b32697a9f68d230c04cd0b5f9aa8044ad42f
Nicolas <nicolasmeireles1004@gmail.com>
2024-07-07 19:58:13 -0300

    :ok_hand: add shadcn/ui.

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button"
+
 function Content() {
     return (
         <div>
             <h1>Content</h1>
+            <Button>Click me</Button>
         </div>
     );
 }
