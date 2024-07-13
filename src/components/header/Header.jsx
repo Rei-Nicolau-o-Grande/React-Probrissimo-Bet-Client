661b20e682fc02467cd80b8445c28400b1bd5624
Nicolas <nicolasmeireles1004@gmail.com>
2024-07-12 21:10:58 -0300

    :ok_hand: add links route in header.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,23 +1,30 @@
-import { Navbar } from "flowbite-react";
+import { Navbar} from "flowbite-react";
+import { NavLink } from "react-router-dom";
+
 
 function Header() {
     return (
         <>
             <Navbar fluid rounded>
-                <Navbar.Brand href="">
-                    <img src="src/assets/img/roleta_da_picanha.jpeg" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
-                    <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-                        Probissimo Bet
-                    </span>
-                </Navbar.Brand>
+                <NavLink to={"/"}>
+                    <Navbar.Brand as={"div"}>
+                        <img src={"src/assets/img/roleta_da_picanha.jpeg"} className="mr-3 h-6 sm:h-9" alt="Probissimo Bet Logo" />
+                        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+                            Probissimo Bet
+                        </span>
+                    </Navbar.Brand>
+                </NavLink>
                 <Navbar.Toggle />
                 <Navbar.Collapse>
-                    <Navbar.Link href="#" active>
+                    <Navbar.Link active>
                         Home
                     </Navbar.Link>
-                    <Navbar.Link href="#">Criar Conta</Navbar.Link>
-                    <Navbar.Link href="#">Entrar</Navbar.Link>
-                    <Navbar.Link href="#">Sair</Navbar.Link>
+                    <Navbar.Link>Criar Conta</Navbar.Link>
+                    <Navbar.Link>Entrar</Navbar.Link>
+                    <Navbar.Link as={"div"}>
+                        <NavLink to={"/user"}>Perfil</NavLink>
+                    </Navbar.Link>
+                    <Navbar.Link className={"text-red-600"}>Sair</Navbar.Link>
                 </Navbar.Collapse>
             </Navbar>
         </>
