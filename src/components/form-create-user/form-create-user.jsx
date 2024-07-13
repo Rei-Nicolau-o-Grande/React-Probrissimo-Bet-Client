6d52e883f772ee78671a225a1016c5ddb311bb2d
Rei-Nicolau-o-Grande <nicolasmeireles1004@gmail.com>
2024-07-13 19:49:54 -0300

    :ok_hand: add react hook form refactor form create user.

--- src/components/form-create-user/form-create-user.jsx
+++ src/components/form-create-user/form-create-user.jsx
@@ -1,84 +1,109 @@
 import { Button, Checkbox, Label, Modal, TextInput } from "flowbite-react";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
+import validator from "validator";
 
 function FormCreateUser() {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors},
+        reset
+    } = useForm();
     const [openModal, setOpenModal] = useState(false);
-    const [username, setUsername] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+
+    const onSubmitCreateUser = (data) => {
+        console.log(data);
+    }
+
+    console.log({ errors })
 
     function onCloseModal() {
         setOpenModal(false);
-        setUsername('');
-        setEmail('');
-        setPassword('');
+        reset();
     }
 
     return (
         <>
             <Button onClick={() => setOpenModal(true)} color={"blue"} className={"my-3"} pill>Criar Conta</Button>
-            <Modal show={openModal} size="md" onClose={onCloseModal} popup>
+            <Modal dismissible show={openModal} size="md" onClose={onCloseModal} popup>
                 <Modal.Header />
                 <Modal.Body>
-                    <div className="space-y-6">
-                        <h3 className="text-xl font-medium text-gray-900 dark:text-white">Criar Conta</h3>
-                        <div>
-                            <div className="mb-2 block">
-                                <Label htmlFor="username" value="Usuário"/>
+                    <form onSubmit={handleSubmit(onSubmitCreateUser)}>
+                        <div className="space-y-6">
+                            <h3 className="text-xl font-medium text-gray-900 dark:text-white">Criar Conta</h3>
+                            <div>
+                                <div className="mb-2 block">
+                                    <Label htmlFor="username" value="Usuário"
+                                       color={`${errors.username ? 'failure' : ''}`}/>
+                                </div>
+                                <TextInput
+                                    id="username"
+                                    type="text"
+                                    placeholder="Usuário"
+                                    color={`${errors.username ? 'failure' : ''}`}
+                                    {...register("username", {required: true})}
+                                    helperText={
+                                        errors?.username?.type === "required" ? 'Campo obrigatório.' : ''
+                                    }
+                                />
                             </div>
-                            <TextInput
-                                id="username"
-                                type="text"
-                                placeholder="Usuário"
-                                value={username}
-                                onChange={(event) => setUsername(event.target.value)}
-                                required
-                            />
-                        </div>
-                        <div>
-                            <div className="mb-2 block">
-                                <Label htmlFor="email" value="Email"/>
+                            <div>
+                                <div className="mb-2 block">
+                                    <Label htmlFor="email" value="Email"
+                                        color={`${errors.email ? 'failure' : ''}`}/>
+                                </div>
+                                <TextInput
+                                    id="email"
+                                    placeholder="email@email.com"
+                                    color={`${errors.email ? 'failure' : ''}`}
+                                    {...register("email",
+                                {required: true,
+                                        validate: (value) => validator.isEmail(value)
+                                    })}
+                                    helperText={
+                                        errors?.email?.type === "required" ? 'Campo obrigatório.' : '' ||
+                                        errors?.email?.type === "validate" ? 'Email inválido.' : ''
+                                    }
+                                />
                             </div>
-                            <TextInput
-                                id="email"
-                                type="email"
-                                placeholder="email@email.com"
-                                value={email}
-                                onChange={(event) => setEmail(event.target.value)}
-                                required
-                            />
-                        </div>
-                        <div>
-                            <div className="mb-2 block">
-                                <Label htmlFor="password" value="Senha"/>
+                            <div>
+                                <div className="mb-2 block">
+                                    <Label htmlFor="password" value="Senha"
+                                        color={`${errors.password ? 'failure' : ''}`}/>
+                                </div>
+                                <TextInput
+                                    id="password"
+                                    type="password"
+                                    placeholder="********"
+                                    color={`${errors.password ? 'failure' : ''}`}
+                                    {...register("password", {required: true, minLength: 3})}
+                                    helperText={
+                                        errors?.password?.type === "required" ? 'Campo obrigatório.' : '' ||
+                                        errors?.password?.type === "minLength" ? 'Mínimo 3 caracteres.' : ''
+                                    }
+                                />
                             </div>
-                            <TextInput
-                                id="password"
-                                type="password"
-                                placeholder="********"
-                                value={password}
-                                onChange={(event) => setPassword(event.target.value)}
-                                required/>
-                        </div>
-                        <div className="flex justify-between">
-                            <div className="flex items-center gap-2">
-                                <Checkbox id="remember"/>
-                                <Label htmlFor="remember">Remember me</Label>
+                            <div className="flex justify-between">
+                                <div className="flex items-center gap-2">
+                                    <Checkbox id="remember"/>
+                                    <Label htmlFor="remember">Remember me</Label>
+                                </div>
+                                <a href="#" className="text-sm text-cyan-700 hover:underline dark:text-cyan-500">
+                                    Lost Password?
+                                </a>
                             </div>
-                            <a href="#" className="text-sm text-cyan-700 hover:underline dark:text-cyan-500">
-                                Lost Password?
-                            </a>
+                            <div className="w-full">
+                                <Button type={"submit"} disabled={Object.keys(errors).length > 0}>Criar</Button>
+                            </div>
+                            <div className="flex justify-between text-sm font-medium text-gray-500 dark:text-gray-300">
+                                Not registered?&nbsp;
+                                <a href="#" className="text-cyan-700 hover:underline dark:text-cyan-500">
+                                    Entrar
+                                </a>
+                            </div>
                         </div>
-                        <div className="w-full">
-                            <Button>Criar</Button>
-                        </div>
-                        <div className="flex justify-between text-sm font-medium text-gray-500 dark:text-gray-300">
-                            Not registered?&nbsp;
-                            <a href="#" className="text-cyan-700 hover:underline dark:text-cyan-500">
-                                Entrar
-                            </a>
-                        </div>
-                    </div>
+                    </form>
                 </Modal.Body>
             </Modal>
         </>
--- src/components/form-login-user/form-login-user.jsx
+++ src/components/form-login-user/form-login-user.jsx
@@ -1,10 +1,16 @@
 import { Button, Checkbox, Label, Modal, TextInput } from "flowbite-react";
 import { useState } from "react";
+import {useForm} from "react-hook-form";
 
 function FormLoginUser() {
+    const { register, handleSubmit} = useForm();
     const [openModal, setOpenModal] = useState(false);
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+
+    const onSubmitLoginUser = (data) => {
+        console.log(data);
+    };
 
     function onCloseModal() {
         setOpenModal(false);
@@ -15,55 +21,59 @@ function FormLoginUser() {
     return (
         <>
             <Button onClick={() => setOpenModal(true)} color={"success"} className={"my-3"} pill>Entar</Button>
-            <Modal show={openModal} size="md" onClose={onCloseModal} popup>
+            <Modal dismissible show={openModal} size="md" onClose={onCloseModal} popup>
                 <Modal.Header />
                 <Modal.Body>
-                    <div className="space-y-6">
-                        <h3 className="text-xl font-medium text-gray-900 dark:text-white">Entrar na sua Conta</h3>
-                        <div>
-                            <div className="mb-2 block">
-                                <Label htmlFor="email" value="Seu email" />
+                    <form>
+                        <div className="space-y-6">
+                            <h3 className="text-xl font-medium text-gray-900 dark:text-white">Entrar na sua Conta</h3>
+                            <div>
+                                <div className="mb-2 block">
+                                    <Label htmlFor="email" value="Seu email"/>
+                                </div>
+                                <TextInput
+                                    id="email"
+                                    type="email"
+                                    placeholder="email@email.com"
+                                    value={email}
+                                    {...register("email")}
+                                    onChange={(event) => setEmail(event.target.value)}
+                                    required
+                                />
                             </div>
-                            <TextInput
-                                id="email"
-                                type="email"
-                                placeholder="email@email.com"
-                                value={email}
-                                onChange={(event) => setEmail(event.target.value)}
-                                required
-                            />
-                        </div>
-                        <div>
-                            <div className="mb-2 block">
-                                <Label htmlFor="password" value="Sua Senha" />
+                            <div>
+                                <div className="mb-2 block">
+                                    <Label htmlFor="password" value="Sua Senha"/>
+                                </div>
+                                <TextInput
+                                    id="password"
+                                    type="password"
+                                    placeholder="********"
+                                    value={password}
+                                    {...register("password")}
+                                    onChange={(event) => setPassword(event.target.value)}
+                                    required/>
                             </div>
-                            <TextInput
-                                id="password"
-                                type="password"
-                                placeholder="********"
-                                value={password}
-                                onChange={(event) => setPassword(event.target.value)}
-                                required />
-                        </div>
-                        <div className="flex justify-between">
-                            <div className="flex items-center gap-2">
-                                <Checkbox id="remember" />
-                                <Label htmlFor="remember">Remember me</Label>
+                            <div className="flex justify-between">
+                                <div className="flex items-center gap-2">
+                                    <Checkbox id="remember"/>
+                                    <Label htmlFor="remember">Remember me</Label>
+                                </div>
+                                <a href="#" className="text-sm text-cyan-700 hover:underline dark:text-cyan-500">
+                                    Lost Password?
+                                </a>
                             </div>
-                            <a href="#" className="text-sm text-cyan-700 hover:underline dark:text-cyan-500">
-                                Lost Password?
-                            </a>
+                            <div className="w-full">
+                                <Button onClick={() => handleSubmit(onSubmitLoginUser)()}>Login</Button>
+                            </div>
+                            <div className="flex justify-between text-sm font-medium text-gray-500 dark:text-gray-300">
+                                Not registered?&nbsp;
+                                <a href="#" className="text-cyan-700 hover:underline dark:text-cyan-500">
+                                    Criar Conta
+                                </a>
+                            </div>
                         </div>
-                        <div className="w-full">
-                            <Button>Login</Button>
-                        </div>
-                        <div className="flex justify-between text-sm font-medium text-gray-500 dark:text-gray-300">
-                            Not registered?&nbsp;
-                            <a href="#" className="text-cyan-700 hover:underline dark:text-cyan-500">
-                                Criar Conta
-                            </a>
-                        </div>
-                    </div>
+                    </form>
                 </Modal.Body>
             </Modal>
         </>
